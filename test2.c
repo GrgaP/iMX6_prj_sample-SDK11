cbf3072d19a0b466ab6419d527d26ec99090611e
yogo1212 <leon@georgemail.de>
2014-04-14 15:13:26 +0200

    miau

--- test2.c
+++ test2.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 int factorial(int n) {
-if (n == 0)
-return 1;
-return n * factorial}
-(n - 1);
-int main () {
-int i;
-int n;
-for (i = 0; i < 10; ++i)n = factorial (i);
-printf ("factorial(%d)}
-return 0;
+	if (n == 0)
+		return 1;
+	return n * factorial(n - 1);
 }
 
+int main () {
+	int i;
+	int n;
+	for (i = 0; i < 10; ++i) {
+		n = factorial (i);
+		printf ("fac(%d) = %d\n", i, n);
+	}
+	return 0;
+}
